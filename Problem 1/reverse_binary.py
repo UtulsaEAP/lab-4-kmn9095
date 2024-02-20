57b725f503669cf358ca76f7d8d787a7de1590f6
Kelsey N <kmn9095@utulsa.edu>
2024-02-20 17:29:57 -0600

    Lab4.1

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -7,9 +7,23 @@ Lab Time:
 """
 
 
-def reverse_binary():
-    user_num = int(input())
-    # YOUR CODE HERE
+#def reverse_binary():
+#    x = int(input())
+#    while x > 1 :
+#        print(str(x % 2))
+#        x=x/2
 
-if __name__ == "__main__":
-    reverse_binary()
+def reverse_binary(n):
+    if n == 0:
+        return '0'
+    
+    result = ''
+    while n > 0:
+        result += str(n % 2)
+        n //= 2
+    
+    return result
+
+# Example usage:
+num = int(input("Enter a positive integer: "))
+print(reverse_binary(num))
