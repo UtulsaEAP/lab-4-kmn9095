d59c4c673472baf59dabfcfd70c8c2855c46a249
Kelsey N <kmn9095@utulsa.edu>
2024-03-01 10:43:25 -0600

    Lab4.sedateme

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -8,18 +8,25 @@ Lab Time:
 """
 
 def brute_eq():
-    #Read in first equation, ax + by = c 
+    # Read in first equation, ax + by = c
     a = int(input())
     b = int(input())
     c = int(input())
-    print("test")
 
-    #Read in second equation, dx + ey = f
+    # Read in second equation, dx + ey = f
     d = int(input())
     e = int(input())
     f = int(input())
 
-    # YOUR CODE HERE
-    
+    # Brute force approach: Check every combination of x and y in the given range
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(f"x = {x}, y = {y}")
+                return
+
+    # If no solution is found
+    print("There is no solution")
+
 if __name__ == "__main__":
     brute_eq()
