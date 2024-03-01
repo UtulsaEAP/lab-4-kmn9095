60b885aa83c36a52447dbdc59db40d79ea4b60fb
Kelsey N <kmn9095@utulsa.edu>
2024-03-01 10:38:13 -0600

    Lab4.Imsodonebro

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -6,10 +6,24 @@ Lab Time:
 """
 
 def inc_5():
-    # Write your code here
-    
+    # Get user input for the two integers
+    num1 = int(input("Enter your first integer: "))
+    num2 = int(input("Enter your second integer: "))
 
+    # Check if num1 is less than or equal to num2
+    if num1 <= num2:
+        # Output the first integer
+        print(num1, end=" ")
 
+        # Output subsequent increments of 5 until reaching num2
+        while num1 + 5 <= num2:
+            num1 += 5
+            print(num1, end=" ")
+
+        # Print a newline at the end
+        print()
+    else:
+        print("Second integer can't be less than the first.")
 
 if __name__ == '__main__':
     inc_5()
