cda7278bcdddc3b55d1c43707cc163f187a2c3c2
Kelsey N <kmn9095@utulsa.edu>
2024-03-01 10:40:51 -0600

    Lab4.whoevencaresmyguy

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -6,8 +6,12 @@ Lab Time:
 """
 
 def reverse_string():
-    # YOUR CODE HERE
-    
+    while True:
+        user_input = input("Enter a line of text (or 'Done' to exit): ")
+        if user_input.lower() in ["done", "d"]:
+            break
+        reversed_text = user_input[::-1]
+        print(reversed_text)
 
 if __name__ == "__main__":
     reverse_string()
